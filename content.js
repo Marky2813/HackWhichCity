ae496b1eb5c0d5d40dc6a44049180ae703a4ec38
Sarthak <messagetosarthak@gmail.com>
2026-04-13 17:16:26 +0530

    Added Mutation Observer and Abort Controller

--- content.js
+++ content.js
@@ -1,18 +1,31 @@
-let cards = document.querySelectorAll(`a[href*=".devfolio.co"]`); 
+let cards; 
+let controller = new AbortController(); 
 
+function attachListeners() {
+  controller.abort(); 
+  controller = new AbortController(); 
 
+  cards = document.querySelectorAll(`a[href*=".devfolio.co"]`);
+  cards.forEach(card => {
+    card.addEventListener("mouseenter", () => {
+      chrome.runtime.sendMessage(
+        { type: 'FETCH_HACKATHON', url: card.href },
+        (response) => {
+          console.log("city:", response.city);
+        }
+      );
+      console.log("hovered:", card.href);
+    }, { signal: controller.signal });
+  });
+}
 
-
-cards.forEach(card => {
-  card.addEventListener("mouseenter", () => {
-    chrome.runtime.sendMessage(
-      {type: 'FETCH_HACKATHON', url: card.href}, 
-      (response) => {
-        console.log("city:", response.city)
-      }
-    )
-    console.log("hovered:", card.href)
-  })
+const observer = new MutationObserver(() => {
+  attachListeners(); 
 })
+
+observer.observe(document.querySelector("main"), {childList:true});
+
+attachListeners(); 
+
 console.log(cards)
 
